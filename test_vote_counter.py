57ce63475d1b63dfd90a98ce1c66c8f399f6ca58
Lando Fabrizio Veramendi Hilario <103605414+Landov311@users.noreply.github.com>
2024-11-13 00:56:19 -0500

    Add files via upload

--- test_vote_counter.py
+++ test_vote_counter.py
@@ -39,6 +39,22 @@ class TestVoteCounter(unittest.TestCase):
         mock_print.assert_any_call("winner is Bob")
         self.assertEqual(mock_print.call_count, 3)
 
+    @patch("builtins.print")
+    def test_count_votes_empate(self, mock_print):
+        # Simulate a CSV file with invalid votes data
+        mock_csv = """city,candidate,votes
+        Springfield,Bob,750
+        Shelbyville,Alice,750"""
+        with patch("builtins.open", mock_open(read_data=mock_csv)):
+            count_votes("votes.csv")
+
+        # Expect Alice to be skipped due to invalid data, only Bob's votes should print correctly
+        mock_print.assert_any_call("Bob: 750 votes")
+        mock_print.assert_any_call("Alice: 750 votes")
+        mock_print.assert_any_call("Existe empate, no hay ganador")
+    
+        self.assertEqual(mock_print.call_count, 3)
+
 if __name__ == "__main__":
     unittest.main()
 
--- vote_counter.py
+++ vote_counter.py
@@ -4,27 +4,32 @@ def count_votes(file_path):
     results = {}
     
     with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        next(reader)  # Skip the header
+        data_votaciones = csv.reader(csvfile, delimiter=',')
+        next(data_votaciones)  # Skip the header
 
-        for row in reader:
-            city = row[0]
+        for row in data_votaciones:
             candidate = row[1]
             try:
             	votes = int(row[2])
             except:
                 votes = 0
             
-            if candidate in results:
-                results[candidate] += votes
-            else:
-                results[candidate] = votes
+            results[candidate] = results.get(candidate, 0) + votes
 
+    print_of_candidates_and_winner(results)
+
+
+
+def print_of_candidates_and_winner(results):
     for candidate, total_votes in results.items():
         print(f"{candidate}: {total_votes} votes")
 
     sortedbyvotes = sorted(results.items(), key=lambda item:item[1], reverse=True)
+    primer_mas_votado=sortedbyvotes[0][1]
+    segundo_mas_votado=sortedbyvotes[1][1]
+    if(primer_mas_votado==segundo_mas_votado):
+        print(f"Existe empate, no hay ganador")
+        return
     print(f"winner is {sortedbyvotes[0][0]}")
-
 # Example usage
 count_votes('votes.csv')
